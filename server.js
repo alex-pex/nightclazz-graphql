3feae098ed055fff0705b8c4980750789de94a64
alex-pex <alexandre.paixao@stadline.com>
2017-12-13 20:38:01 +0100

    request opendata api

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
+const fs = require('fs');
+const rp = require('request-promise');
 
 // Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+const schema = buildSchema(fs.readFileSync('./schema.gql').toString());
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => 'Hello world!'
+  search: async ({ query }) => {
+    const uri = `https://api-adresse.data.gouv.fr/search/?q=${query}`;
+    console.log('REQUEST', uri);
+
+    const adresseResponse = await rp({
+      uri,
+      json: true // Automatically parses the JSON string in the response
+    });
+    const firstResult = adresseResponse.features[0] || {};
+    const firstProperties = firstResult.properties || {};
+
+    return {
+      city: firstProperties.city,
+      zipCode: firstProperties.postcode
+    };
+  }
 };
 
 const app = express();
